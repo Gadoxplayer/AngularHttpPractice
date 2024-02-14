c35466e3f1001d03fb07ef11c55384bfea0aa67f
EduardoCampo <eduardocampo91@gmail.com>
2024-02-14 00:30:51 -0500

    using get methjod to get datra trought observables operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPost();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
-    this.http.post(
-      'https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json', 
-      postData
-      ).subscribe(responseData => {
+    this.http
+      .post(
+        "https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json",
+        postData
+      )
+      .subscribe((responseData) => {
         console.log(responseData);
-        
       });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPost()
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPost() {
+    this.http
+      .get("https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json")
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if(responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe((post) => {
+        //..
+        console.log(post);
+      });
   }
 }
