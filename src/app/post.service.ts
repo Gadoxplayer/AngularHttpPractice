d57c58d5a5dcb296ac843b54ca90bd9315ce7e0d
EduardoCampo <eduardocampo91@gmail.com>
2024-02-15 22:07:19 -0500

    obser diferent tyupes of response

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
 @Injectable({ providedIn: "root" })
@@ -13,55 +13,78 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
-    const postData: Post = {title: title, content: content}
+    const postData: Post = { title: title, content: content };
     console.log(postData);
     this.http
       .post<{ name: string }>( //this can be set in a Service for better code
         "https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json",
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
-      .subscribe((responseData) => {
-        console.log(responseData);
-      }, error => {
-        this.error.next(error.message);
-      });
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPost() {
     let SearchParams = new HttpParams();
     SearchParams = SearchParams.append("print", "pretty");
     SearchParams = SearchParams.append("custom", "key");
-    return this.http
-      .get<{[key: string]: Post}>("https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json",
-      {
-        headers: new HttpHeaders({
-          "custom-Header": "Hello"
-        }),
-        //params: new HttpParams().set('print', 'pretty')
-        params: SearchParams
-      }
-      )
-      //.pipe(map((responseData: {[key: string]: Post}) => {
-      .pipe( //this can be set in a Service for better code
-        map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if(responseData.hasOwnProperty(key)){
-          postsArray.push({...responseData[key], id: key})
+    return (
+      this.http
+        .get<{ [key: string]: Post }>(
+          "https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json",
+          {
+            headers: new HttpHeaders({
+              "custom-Header": "Hello",
+            }),
+            //params: new HttpParams().set('print', 'pretty')
+            params: SearchParams,
           }
-        }
-        return postsArray;
-      }),
-      catchError(errorMessage => {
-        return throwError(errorMessage);
-      })
-      );
+        )
+        //.pipe(map((responseData: {[key: string]: Post}) => {
+        .pipe(
+          //this can be set in a Service for better code
+          map((responseData) => {
+            const postsArray: Post[] = [];
+            for (const key in responseData) {
+              if (responseData.hasOwnProperty(key)) {
+                postsArray.push({ ...responseData[key], id: key });
+              }
+            }
+            return postsArray;
+          }),
+          catchError((errorMessage) => {
+            return throwError(errorMessage);
+          })
+        )
+    );
     //   .subscribe((post) => {
     //     //..
     //   });
     // since im returning this now no http request is send because there is none interested, need tosubscribe to it in the component i need it
   }
   deletePost() {
-    return this.http.delete('https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json');
+    return this.http.delete(
+      "https://ng-practice-ac176-default-rtdb.firebaseio.com/post.json",
+      {
+        observe: 'events'
+      }
+    ).pipe(tap(event => {
+      console.log(event);
+      if(event.type === HttpEventType.Sent) {
+        console.log(event.type);
+      }
+      if(event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }));
   }
 }
