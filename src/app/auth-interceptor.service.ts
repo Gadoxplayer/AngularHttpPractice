619b9e9c06d0ada93b47c22e0e2b11195ec52f2d
EduardoCampo <eduardocampo91@gmail.com>
2024-02-25 22:41:46 -0500

    adding multiple interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,14 +1,33 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('reques is on its way');
-        console.log(req.url);
-        
-        const modifiedReq = req.clone({
-            headers: req.headers.append('Auth', 'xyz')
-        })
-        return next.handle(modifiedReq);        
-    }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // console.log("reques is on its way");
+    // console.log(req.url);
+
+    const modifiedReq = req.clone({
+      headers: req.headers.append("Auth", "xyz"),
+    });
+    return next.handle(modifiedReq);
+    // .pipe(
+    //     tap(event => {
+    //     console.log(event);
+    //     if (event.type === HttpEventType.Response) {
+    //         console.log('response arrived, body data: ', event.body);
+
+    //     }
+    // })
+    // );
+  }
 }
